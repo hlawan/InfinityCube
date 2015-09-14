17f2be0e03a3ecd3578acd2430bb0ae1d7d94431
Jonas Winter <winterjo1991@gmail.com>
2015-09-14 14:29:03 +0200

    moved all cube stuff to new file

--- src/infinitycube/generator.go
+++ src/infinitycube/generator.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-    "net"
     "math/rand"
-    "io"
     //"fmt"
     "time"
     "github.com/lucasb-eyer/go-colorful"
@@ -61,36 +59,6 @@ func (i *IntervalTicker) Tick(d time.Duration, o interface{}) {
         i.Last = d
     }
     i.Consumer.Tick(d, fire)
-}
-
-type CubeX struct {
-    io.ReadWriter
-    buffer [3 * EDGE_LENGTH * EDGES_PER_SIDE * NR_OF_SIDES]byte
-}
-
-func NewCubeX() (c *CubeX, err error) {
-	socketCon, err := net.Dial("tcp", "192.168.1.222:12345")
-	if err != nil {
-		return
-    }
-    c = &CubeX{ReadWriter: socketCon}
-    return
-}
-
-func (c *CubeX) Tick(d time.Duration, o interface{}) {
-    leds := o.([]Led)
-
-    h := 0
-    for i, _ := range leds {
-        c.buffer[h+0], c.buffer[h+1], c.buffer[h+2] = leds[i].Color.RGB255()
-        h += 3
-    }
-
-	var startByte [1]byte
-    n, _ := c.Read(startByte[:])
-    if(n == 1) {
-        c.Write(c.buffer[:])
-    }
 }
 
 type RandomTicker struct {
