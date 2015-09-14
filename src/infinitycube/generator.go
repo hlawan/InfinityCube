263bb93b66159e51f00994ed42ebf472b0830de8
Jonas Winter <winterjo1991@gmail.com>
2015-09-14 10:00:14 +0200

    bugfix: now using BlendHsv() instead of BlendLab() in blurfilter...less weird stuff happens

--- src/infinitycube/generator.go
+++ src/infinitycube/generator.go
@@ -134,10 +134,10 @@ func (f *DirtyBlurFilter) Tick(d time.Duration, o interface{}) {
     for i, _ := range leds {
         s := .02
         c := leds[i].Color
-        c = c.BlendLab(leds[idx(i, -2)].Color, s/4)
-        c = c.BlendLab(leds[idx(i, -1)].Color, s)
-        c = c.BlendLab(leds[idx(i,  1)].Color, s)
-        c = c.BlendLab(leds[idx(i,  2)].Color, s/4)
+        c = c.BlendHsv(leds[idx(i, -2)].Color, s/4)
+        c = c.BlendHsv(leds[idx(i, -1)].Color, s)
+        c = c.BlendHsv(leds[idx(i,  1)].Color, s)
+        c = c.BlendHsv(leds[idx(i,  2)].Color, s/4)
         f.Leds[i].Color = c
     }
     f.Consumer.Tick(d, f.Leds[:])
