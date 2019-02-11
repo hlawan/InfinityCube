6233884bbe0f58ffb9a05b2c25cf6ce70ed4fbd6
Jonas Winter <winterjo1991@gmail.com>
2019-02-11 22:51:09 +0100

    added colorGradient generator

--- src/infinitycube/colorGenerator.go
+++ src/infinitycube/colorGenerator.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -91,4 +92,69 @@ func (hsv *HsvFade) Colorize(leds []Color) []Color {
 	}
 
 	return colLeds
+}
+
+type ColorGradient struct {
+	Elements []Color
+	length   int
+}
+
+func NewColorGradient(colorElements []Color, gradientLength int) *ColorGradient {
+
+	if len(colorElements) < 2 {
+		fmt.Println("not enough colors for color gradient")
+		return nil
+	}
+
+	cg := &ColorGradient{
+		Elements: colorElements,
+		length:   gradientLength}
+
+	return cg
+}
+
+func (cg *ColorGradient) Update() {
+
+}
+
+func (grad *ColorGradient) Colorize(leds []Color) []Color {
+
+	var colLeds []Color
+
+	for i, led := range leds {
+
+		colLed := linearGradient(i%EDGE_LENGTH, EDGE_LENGTH, grad.Elements[0], grad.Elements[1])
+
+		// Gamma correction
+		colLed.V = correctGamma(led.V)
+
+		colLeds = append(colLeds, colLed)
+	}
+
+	return colLeds
+}
+
+func linearGradient(ledPosition, length int, col1, col2 Color) Color {
+
+	x := float64(ledPosition)
+	xMax := float64(length)
+
+	r1, g1, b1 := col1.RGB()
+	r2, g2, b2 := col2.RGB()
+
+	r1f := float64(r1)
+	g1f := float64(g1)
+	b1f := float64(b1)
+	r2f := float64(r2)
+	g2f := float64(g2)
+	b2f := float64(b2)
+
+	nR := uint8(r1f + (x/xMax)*(r2f-r1f))
+	nG := uint8(g1f + (x/xMax)*(g2f-g1f))
+	nB := uint8(b1f + (x/xMax)*(b2f-b1f))
+
+	var newCol Color
+	newCol.FromRGB(nR, nG, nB)
+
+	return newCol
 }
